a2a16db5c57b1d341e2407d1dcc23acc5933dd42
ShameerKhan1 <107603954+ShameerKhan1@users.noreply.github.com>
2022-10-27 13:33:16 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+async function movienight() {
+  await cookpopcorn();
+  await pourdrinks();
+  await startmovie();
+}
+function cookpopcorn() {
+  return Promise();
+}
+function pourdrinks() {}
+function startmovie() {}
